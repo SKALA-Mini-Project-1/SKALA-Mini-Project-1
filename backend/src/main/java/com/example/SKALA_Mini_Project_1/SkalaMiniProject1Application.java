684e13d9b02325f45fe49a14059fa1ce93e073f4
l-wanderer01 <jhjjang0423@naver.com>
2026-02-10 20:21:54 +0900

    [docs] 프로젝트 환경 세팅

---
+++ backend/src/main/java/com/example/SKALA_Mini_Project_1/SkalaMiniProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.SKALA_Mini_Project_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkalaMiniProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkalaMiniProject1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/SKALA_Mini_Project_1/SkalaMiniProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SKALA_Mini_Project_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkalaMiniProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
